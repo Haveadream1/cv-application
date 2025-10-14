4f5391ab6d93791e52d8f6dfae2fbfb0d2270619
Haveadream1 <260602david@gmail.com>
2025-10-14 17:09:28 +0200

    feat: import cv css

--- src/components/CvPage.jsx
+++ src/components/CvPage.jsx
@@ -1,6 +1,9 @@
+import '../styles/CvPage.css'
+
 export default function Cv() {
     return (
         <div className="cv-container">
+            <div className="cv-header"></div>
             <div className="personal-details-section"></div>
             <div className="education-section"></div>
             <div className="experience-section"></div>
