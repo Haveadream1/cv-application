0879d3990dd2022dbeaa6d6c389d14a82a7dbb4b
Haveadream1 <260602david@gmail.com>
2025-10-14 16:46:49 +0200

    feat: add cv structure

---
+++ src/components/CvPage.jsx
@@ -0,0 +1,9 @@
+export default function Cv() {
+    return (
+        <div className="cv-container">
+            <div className="personal-details-section"></div>
+            <div className="education-section"></div>
+            <div className="experience-section"></div>
+        </div>
+    )
+}
