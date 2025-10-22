601a840cd2e17b5f06ebd052b056f04429ffbeb9
Haveadream1 <260602david@gmail.com>
2025-10-22 10:13:49 +0200

    refactor: section function to handle skill class

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,13 +2,17 @@ export default function Section({
     children,
     sectionClass,
     id,
-    headingText
+    headingText,
+    contentClass = "content-style"
 }) {
     return (
         <section className={sectionClass} aria-labelledby={id}>
             <h3 id={id} className="content-heading">{headingText}</h3>
-            {children}
+            <div className={contentClass}>
+                {children}
+            </div>
         </section>
     );
 }
 // Depening on the section add the content
+// We pass the default style contentClass as only one section have a different one
