cc4e3c03e58646eedbf4242b724ddcf66e7c49f9
Haveadream1 <260602david@gmail.com>
2025-10-23 10:36:16 +0200

    feat: create form container

---
+++ src/containers/FormContainer.js
@@ -0,0 +1,11 @@
+import Form from "../components/Form";
+import PersonalInfosFieldset from "../components/FieldsetFolder/PersonalInfosFieldset";
+
+export default function FormContainer() {
+    return (
+        <Form>
+            <PersonalInfosFieldset />
+            
+        </Form>
+    );
+}
