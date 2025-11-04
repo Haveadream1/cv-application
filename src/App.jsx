c4b58c57c02f3d42be13adaf6026c761ccc4b4c2
Haveadream1 <260602david@gmail.com>
2025-11-04 10:36:29 +0100

    feat: handle form and display results

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import FormContainer from './containers/FormContainer.jsx'
+import PreviewContainer from './containers/PreviewContainer.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const [formData, setFormData] = useState({
+        personalInfo : {
+            fullName: "",
+            email: "YSeung@naver.com",
+            phone: "+82 XX XXXX YYYY",
+            address: "Seoul, Jongno-gu, Sajik-ro-3-gil 23",
+        },
+        experience : { // Here handle the concat of strings
+            jobTitle: "Front-end developer at KT company",
+            companyName: "KT company",
+            startDate: "2025 - Present",
+            endDate: "Present",
+            jobDescription: "In charge of building the visible, interactive part of a website or application by using HTML, CSS, and JavaScript"
+        },
+        education : {
+            institution: "Yeungnam University",
+            degree: "Computer science bachelor",
+            graduationDate: "February 2022"
+        },
+        skills : {
+            skill: "Eager to learn" // Make an array ?
+        }
+    });
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handleDataChange = (section, field, value) => {
+        setFormData(prev => ({
+            ...prev, // ...prev copy the current state for el unchanged
+            [section]: {
+                ...prev[section],
+                [field]: value // access the value to change
+            }
+        }));
+    }
+
+    return (
+        <>
+            <FormContainer formData={formData} onDataChange={handleDataChange} />
+            <PreviewContainer formData={formData} />
+        </>
+    );
 }
-
-export default App
