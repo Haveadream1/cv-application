266dd1c80cd0796c34fe2a09eda4b1fde797c6aa
Haveadream1 <260602david@gmail.com>
2025-11-05 21:44:00 +0100

    feat: handle form default submit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,12 @@
 export default function Form({ children }) {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
     return (
         <aside role="complementary" aria-label="Form to provide information for the curriculum vitae">
-            <form id="form" action="post">
+            <form id="form" action="post" onSubmit={handleSubmit}>
                 {children}
-                <button type="submit" aria-label="submit-form">Submit</button>
+                <button type="submit" className="button" aria-label="submit-form">Submit</button>
             </form>
         </aside>
     );
